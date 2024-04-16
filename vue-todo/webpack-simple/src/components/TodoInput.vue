83682c8d51a8825679976aaa39f37216cb081143
eeunha <eunha.dev@gmail.com>
2024-04-17 00:13:56 +0900

    todo list local storage 데이터 저장

---
+++ vue-todo/webpack-simple/src/components/TodoInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo">추가</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+    },
+  },
+};
+</script>
+
+<style></style>
